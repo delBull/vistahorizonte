9600adceb7a9f7ab3f2aba35e73008e68542cc2a
delBull <marco.munoz9@gmail.com>
2024-02-19 23:36:52 -0600

    favicon

--- pages/index.js
+++ pages/index.js
@@ -28,21 +28,21 @@ export default function Home() {
                                 </div>
                             </div>
                             <h1>Coming soon</h1>
-                            <p>Mientras tanto deja tu email para mantenerte al tanto</p>
+                           <p>Mientras tanto deja tu email para mantenerte al tanto</p>
                             <fieldset className="email">
                                 <input type="email" className="style-1" id="email" placeholder="Email*" name="email" tabIndex={2} aria-required="true" required />
                             </fieldset>
-                            <Link href="#" className="tf-button style-1 h50">Subscríbete<i className="icon-arrow-up-right2" /></Link>
-                        </div>
-                        <div className="widget-social">
+                            <Link href="#" className="tf-button style-1 h50">Subscríbete<i className="icon-arrow-up-right2" /></Link> 
+                        </div> 
+                       <div className="widget-social">
                             <ul className="flex justify-center">
                                 <li><Link href="#" className="icon-facebook" /></li>
                                 <li><Link href="#" className="icon-instagram" /></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            </ul> 
+                        </div> 
+                    </div> 
+                </div> 
+            </div> 
 
         </>
     )
